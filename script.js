eb17130704e408776381ffbccbd793b579b88d12
milkstard <jmarkcarbs@gmail.com>
2022-05-07 01:23:23 +0800

    Add 2 function game() and playGame()
        The game() function just calls the playGame() and playGame() do loops for the game

--- script.js
+++ script.js
@@ -19,7 +19,49 @@ function resultPlay(playerSelection, computerSelection){
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(computerSelection);
-console.log(resultPlay(playerSelection,computerSelection));
+function playRound(){
+    
+    let resultFinalPS = 0;
+    let resultFinalCP = 0;
+
+    for (let i = 1; i <= 5; i++){
+        //my code
+        let playerSelection = prompt("Enter rock, paper, scissor", "");
+
+        while(playerSelection.toLowerCase() != "rock" && playerSelection.toLowerCase() != "paper" && playerSelection.toLowerCase() != "scissor"){
+            
+            if(playerSelection === null){
+               break;
+            }
+            console.log(playerSelection);
+            playerSelection = prompt("Info is not acceptable");
+            
+        }
+
+        let computerSelection = computerPlay();
+        console.log("Player 1: " + playerSelection + " Computer 1: " + computerSelection);
+        let partialResult = resultPlay(playerSelection, computerSelection);
+
+        if(partialResult.includes("Win")){
+            resultFinalPS++;
+        }
+        else if(partialResult.includes("Lose")){
+            resultFinalCP++;
+        } //no score added if its a TIE!
+
+        console.log("Score: " + resultFinalPS + " || " + resultFinalCP);
+
+    }
+
+}
+
+function game(){
+    playRound();
+}
+
+
+game();
+//const playerSelection = "rock";
+//const computerSelection = computerPlay();
+//console.log(computerSelection);
+//console.log(resultPlay(playerSelection,computerSelection));
